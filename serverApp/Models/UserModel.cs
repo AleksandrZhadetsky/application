8a0e9e7a088656389b636c1dffa1acb97990133f
Aleksandr Zhadetsky <coolweak2@gmail.com>
2025-01-06 00:23:00 +0400

    initial commit

--- serverApp/Models/UserModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace application.Server.Models
-{
-    public class UserModel
-    {
-        public string Email { get; set; }
-        public string Name { get; set; }
-        public Guid Password { get; set; }
-    }
-}
--- serverApp/Services/IFileOperationsService.cs
+++
@@ -1,9 +0,0 @@
-﻿using application.Server.Models;
-
-namespace application.Server.Services
-{
-    public interface IFileOperationsService
-    {
-        Task<Node> GetJsonFileContent(string path);
-    }
-}
